d993151dd665083589a06d99890f768b83e62a57
Oliver Newman <oliver@whmsi.com>
2018-02-27 21:23:06 -0800

    Fix some stupid mistakes
    In particular, I was setting the value of the 'Authorization' header to
    be of the form 'Bearer: <access_token>', *with* a colon. Oops.

--- spotify_archiver/service.py
+++ spotify_archiver/service.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import base64
-import json
 import os
 
 import requests
@@ -8,59 +7,58 @@ import requests
 BASE_URL = 'https://api.spotify.com/v1'
 
 
+class SpotifyError(RuntimeError):
+    pass
+
+
 def handler(event=None, context=None):
     session = get_authenticated_session()
 
     my_id = get_my_id(session)
 
-    discover_weekly_uri, archive_uri = get_playlist_uris(session, my_id)
+    discover_weekly_id, archive_id = get_playlist_ids(session, my_id)
 
-    discover_weekly_tracks = get_playlist_tracks(session, my_id,
-                                                 discover_weekly_uri)
+    discover_weekly_tracks = get_playlist_tracks(
+        session, user_id='spotify', playlist_id=discover_weekly_id)
 
-    add_tracks_to_playlist(session, my_id, archive_uri, discover_weekly_tracks)
+    add_tracks_to_playlist(session, my_id, archive_id, discover_weekly_tracks)
 
 
 def get_my_id(session):
     response = session.get(BASE_URL + '/me')
-
     me = response.json()
 
     return me['id']
 
 
-def get_playlist_uris(session, my_id):
-    discover_weekly_uri, archive_uri = None, None
+def get_playlist_ids(session, my_id):
+    discover_weekly_id, archive_id = None, None
     next_page_url = BASE_URL + '/me/playlists'
 
-    while next_page_url and not (discover_weekly_uri and archive_uri):
-        response = session.get('https://api.spotify.com/v1/me/playlists')
+    while next_page_url and not (discover_weekly_id and archive_id):
+        response = session.get(next_page_url)
 
         playlists = response.json()['items']
 
         for playlist in playlists:
-            # Includes followed playlists, so owner isn't necessarily me
-            if playlist['owner']['id'] != my_id:
-                continue
+            owner_id = playlist['owner']['id']
+            name = playlist['name']
 
-            if playlist['name'] == 'Discover Weekly':
-                discover_weekly_uri = playlist['uri']
-            elif playlist['name'] == 'Discover Weekly Archive':
-                archive_uri = playlist['uri']
+            if owner_id == 'spotify' and name == 'Discover Weekly':
+                discover_weekly_id = playlist['id']
+            elif owner_id == my_id and name == 'Discover Weekly Archive':
+                archive_id = playlist['id']
 
-            if discover_weekly_uri and archive_uri:
-                break
+            if discover_weekly_id and archive_id:
+                return discover_weekly_id, archive_id
 
         next_page_url = response.json()['next']
 
-    if not discover_weekly_uri or not archive_uri:
-        raise RuntimeError('Unable to find Discovery Weekly and Discover '
-                           'Weekly Archive playlists in user\'s account.')
-
-    return discover_weekly_uri, archive_uri
+    raise SpotifyError('Unable to find Discovery Weekly and Discover Weekly '
+                       'Archive playlists in user\'s account.')
 
 
-def get_playlist_tracks(session, user_id, playlist_uri):
+def get_playlist_tracks(session, user_id, playlist_id):
     url = BASE_URL + f'/users/{user_id}/playlists/{playlist_id}/tracks'
     response = session.get(url)
 
@@ -69,19 +67,19 @@ def get_playlist_tracks(session, user_id, playlist_uri):
     return [track['track'] for track in playlist_tracks]
 
 
-def add_tracks_to_playlist(session, user_id, playlist_uri, tracks):
-    track_uris = [track['uri'] for track in tracks]
+def add_tracks_to_playlist(session, user_id, playlist_id, tracks):
+    # At most 100 tracks can be passed in one request (DW is generally only 30
+    # tracks long)
+    track_uris = [track['uri'] for track in tracks][:100]
 
     payload = {
-        'uris': ','.join(track_uris)
+        'uris': track_uris,
+        'position': 0,
     }
     url = BASE_URL + f'/users/{user_id}/playlists/{playlist_id}/tracks'
+    response = session.post(url, json=payload)
 
-    response = session.post(url, data=payload)
-
-    error = response.json().get('error_description')
-    if error:
-        raise requests.HTTPError(error_description, response=response)
+    assert response.status_code == 201, response.json()['error']
 
 
 def get_authenticated_session():
@@ -104,13 +102,13 @@ def get_authenticated_session():
         'grant_type': 'refresh_token',
         'refresh_token': refresh_token,
     }
-
-    response = session.post('https://accounts.spotify.com/api/token', payload)
+    url = 'https://accounts.spotify.com/api/token'
+    response = session.post(url, payload)
 
     assert response.status_code == 200, response.text
 
     access_token = response.json()['access_token']
-    session.headers['Authorization'] = f'Bearer: {access_token}'
+    session.headers['Authorization'] = f'Bearer {access_token}'
     session.headers['Content-Type'] = 'application/json'
 
     return session
